224363fafc2dbf8ffde2015a05a8dc1e9aa11350
miriamfrank23 <miriamfrank23@gmail.com>
2018-11-27 13:27:58 -0500

    user post working with updated model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_26_042913) do
+ActiveRecord::Schema.define(version: 2018_11_27_181417) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 2018_11_26_042913) do
     t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
