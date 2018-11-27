49b64816b4fd731dc2c226c47c214b6d61684f47
miriamfrank23 <miriamfrank23@gmail.com>
2018-11-27 10:58:01 -0500

    added validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
   has_many :comments
   has_many :books, through: :comments
+
+  has_secure_password
+  validates :first_name, :email, :password, presence: true
+  validates :email, uniqueness: { case_sensitive: false }
 end
